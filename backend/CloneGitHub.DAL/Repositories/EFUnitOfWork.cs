47b32af405d51b17af0e51a624df6c8e1e759a02
Dn. Shvts <thedanny2005@gmail.com>
2025-04-09 13:27:44 +0300

    add UnitOfWork realization

---
+++ backend/CloneGitHub.DAL/Repositories/EFUnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using CloneGitHub.DAL.EF;
+using CloneGitHub.DAL.Entities;
+using CloneGitHub.DAL.Interfaces;
+using CloneGitHub.DAL.Repositories;
+
+public class EFUnitOfWork : IUnitOfWork
+{
+    private readonly CloneGitHubContext _context;
+
+    public EFUnitOfWork(CloneGitHubContext context)
+    {
+        _context = context;
+        Repositories = new Repository<Repository>(_context);
+        Activities = new Repository<Activity>(_context);
+        Users = new Repository<User>(_context);
+        Subscriptions = new Repository<Subscription>(_context);
+    }
+
+    public IRepository<Repository> Repositories { get; }
+    public IRepository<Activity> Activities { get; }
+    public IRepository<User> Users { get; }
+    public IRepository<Subscription> Subscriptions { get; }
+
+    public async Task<int> CompleteAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+}
