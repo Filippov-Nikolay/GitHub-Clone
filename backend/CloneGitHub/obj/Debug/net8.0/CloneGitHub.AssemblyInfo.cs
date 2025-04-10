d278097fc93e00de4be7f8d60ad8806aefd8312f
dimathecreator <dzhosan777@gmail.com>
2025-04-10 15:05:22 +0200

    added Services BLL

--- backend/CloneGitHub/obj/Debug/net8.0/CloneGitHub.AssemblyInfo.cs
+++ backend/CloneGitHub/obj/Debug/net8.0/CloneGitHub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloneGitHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47b32af405d51b17af0e51a624df6c8e1e759a02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7063eace548e9b7715992d683dfa9a5c395a1780")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloneGitHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloneGitHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
