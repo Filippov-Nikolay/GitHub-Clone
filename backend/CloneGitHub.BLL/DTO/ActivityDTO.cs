d835efb89d362a6c7999873b5d974dc16bdc1757
Filippov-Nikolay <filippovnikolay195@gmail.com>
2025-04-16 14:33:13 +0300

    Change BLL (UserDetails) for Dima

--- backend/CloneGitHub.BLL/DTO/ActivityDTO.cs
+++ backend/CloneGitHub.BLL/DTO/ActivityDTO.cs
@@ -1,3 +1,4 @@
+using CloneGitHub.DAL.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ namespace CloneGitHub.BLL.DTO
 {
     public class ActivityDTO
     {
-         public int Id { get; set; }
+        public int Id { get; set; }
         public int RepositoryId { get; set; }
         public DateTime ChangedDate { get; set; }
 
--- backend/CloneGitHub.BLL/Infrastructure/UnitOfWorkServiceExtensions.cs
+++ backend/CloneGitHub.BLL/Infrastructure/UnitOfWorkServiceExtensions.cs
@@ -4,9 +4,9 @@ using CloneGitHub.DAL.Repositories;
 
 namespace CloneGitHub.BLL.Infrastructure
 {
-    public class UnitOfWorkServiceExtensions
+    public static class UnitOfWorkServiceExtensions
     {
-           public static void AddUnitOfWorkService(this IServiceCollection services)
+        public static void AddUnitOfWorkService(this IServiceCollection services)
         {
             services.AddScoped<IUnitOfWork, EFUnitOfWork>();
         }
