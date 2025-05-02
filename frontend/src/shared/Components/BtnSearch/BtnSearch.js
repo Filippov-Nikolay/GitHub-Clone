9e78229b2586d1d6189fa04b8848be6e53b8f705
Filippov-Nikolay <filippovnikolay195@gmail.com>
2025-05-02 06:35:42 +0300

    fix adaptive for page Stars and fix style for Nav

--- frontend/src/shared/Components/BtnSearch/BtnSearch.js
+++ frontend/src/shared/Components/BtnSearch/BtnSearch.js
@@ -5,10 +5,10 @@ export default function BtnSearch({ btnClick = null, svgComponent, btnText, addi
     return (
         <button className="btn-search" onClick={btnClick} style={{ backgroundColor: bgColor }}>
             <div className="btn-search__wrapper">
-                <span className="btn-icon__span btn-icon__span--search">{svgComponent}</span>
-                <span className="nav__btn-text">{btnText}</span>
+                <span className="btn-search__span btn-search__span--search">{svgComponent}</span>
+                <span className="btn-search__text">{btnText}</span>
             </div>
-            {additionalSvgComponent && <span className="btn-icon__span">{additionalSvgComponent}</span>}
+            {additionalSvgComponent && <span className="btn-search__span">{additionalSvgComponent}</span>}
         </button>
     )
 }
